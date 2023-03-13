007b8d8005c3d13e530ca8006b86f8f28ab742ba
mi333xd <miguelgomezmartinez12@gmail.com>
2023-03-13 18:58:54 +0100

    Test de FizzBuzz hasta el 6

--- TareaTDD/src/FizzBuzzTeat.java
+++ TareaTDD/src/FizzBuzzTeat.java
@@ -1,3 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class FizzBuzzTeat {
-
+    @Test
+    public void testUno(){
+        String result = FizzBuzz.getFizzBuzz(1);
+        assertEquals("1", result);
+    }
+    @Test
+    public void testDos() {
+        assertEquals("2", FizzBuzz.getFizzBuzz(2));
+    }
+    @Test
+    public void testTres() {
+        assertEquals("Fizz", FizzBuzz.getFizzBuzz(3));
+    }
+    @Test
+    public void testCuatro(){
+        assertEquals("4", FizzBuzz.getFizzBuzz(4));
+    }
+    @Test
+    public void testCinco(){
+        assertEquals("Buzz", FizzBuzz.getFizzBuzz(5));
+    }
+    @Test
+    public void testSeis(){
+        assertEquals("Fizz", FizzBuzz.getFizzBuzz((6)));
+    }
 }
